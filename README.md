5683fa29e1986a3c4953e8ade139def654ca1502
Ilmari Heikkinen <hei@heichen.hk>
2019-06-30 01:58:16 +0800

    HTTP2, cached files, config file, bin

--- README.md
+++ README.md
@@ -1,44 +1,196 @@
-# Qframe
+# Qframe 0.9.1
 
-Code-centric JSON API framework in 300 lines of easy-to-customize code.
+Front-end friendly JSON API server in 300 lines of code.
+Ideal for use with something like `create-react-app`.
 
 Uses PostgreSQL as the database.
 
 
 ## Features
 
- * User accounts
- * Users have multiple items with CRUD
- * All API request bodies are JSON, all API responses are JSON
+ * User accounts using email & password for auth
+ * User sessions
+ * Users own items from item table with CRUD
+ * JSON `data` field for storing user and item details
  * Routing
  * DB migrations
- * Multi-process cluster server with auto-restart
- * Fast iteration using nodemon to restart server on changes
- * Full SQL
- * Static file serving
- 
+ * Static file serving from cached memory buffers with `chokidar` change watcher
+ * Multi-process cluster server with auto-restart on error (not HA though)
+ * Fast server dev iteration using nodemon to restart server on changes
+ * Full SQL support from `node-postgres`
+ * Support for serving over HTTP, HTTPS & HTTP/2
+ * Config from file or env var
+
+Performance testing on a 4-core MBP, using `bombardier -n 1000000 -c 300 http://localhost:8000/`
+
+ * Static file over HTTP, ~80 kreqs/s 
+ * `/_/items/view` DB read over HTTP/HTTPS, ~12 kreqs/s
+ * Around 60k static file HTTPS reqs per second
+
 
 ## Usage
 
-Create database and start server
+Default API server, runs on port 8000.
 
 ```bash
-    $ createdb qframe
-    $ yarn && yarn nodemon .
+$ createdb qframe
+$ npx qframe
 ```
 
-Open http://localhost:8000 in your browser and try out the API in the console
+Open http://localhost:8000 in your browser and try out the built-in API in the console
 
 ```javascript
-    var headers = {};
-    var get = async(path) => (await fetch(path, {headers})).json();
-    var post = async(path, body) => (await fetch(path, {method:'POST', headers, body: JSON.stringify(body)})).json();
-    var user = await post('/user/create', {email:'foo@bar', name: 'foo', password: 'bar'});
-    headers = await post('/user/authenticate', {email: 'foo@bar', password: 'bar'});
-    var userData = await post('/user/edit/data', {avatar: "pirate", favouriteQuote: "Pinata Pirata!"});
-    var {id} = await post('/items/create', {name: "My wonderful item", wonderfulness: 9999});
-    var item = await get(`/items/view/${id}`);
-    await post('/items/edit', {id, data: {name: "My wonderful item", wonderfulness: 10000}})
-    var items = await get(`/items/list`);
-    console.log(user, headers, userData, item, items);
+var headers = {};
+var get = async(path) => (await fetch(path, {headers})).json();
+var post = async(path, body) => (await fetch(path, {method:'POST', headers, body: JSON.stringify(body)})).json();
+var user = await post('/_/user/create', {email:'foo@bar', name: 'foo', password: 'bar'});
+headers = await post('/_/user/authenticate', {email: 'foo@bar', password: 'bar'});
+var userData = await post('/_/user/edit/data', {avatar: "pirate", favouriteQuote: "Pinata Pirata!"});
+var {id} = await post('/_/items/create', {name: "My wonderful item", wonderfulness: 9999});
+var item = await get(`/_/items/view/${id}`);
+await post('/_/items/edit', {id, data: {name: "My wonderful item", wonderfulness: 10000}})
+var items = await get(`/_/items/list`);
+console.log(user, headers, userData, item, items);
+```
+
+Or you can jump straight in and make your own custom server.
+
+```bash
+$ git clone https://github.com/kig/qframe
+$ cd qframe
+$ createdb qframe
+$ yarn
+$ yarn nodemon .
+# Edit index.js to customize your server
+```
+
+But maybe you can do your thing with the handy config system!
+
+```bash
+$ createdb my_db
+$ QFRAME_CONFIG='{"port": 8888, "pg":{"database": "my_db"}, "root": "/var/www/html"}' npx qframe 
+# Or use a config file
+$ echo '{"port": 8888, "pg":{"database": "my_db"}, "root": "/var/www/html"}' > qframe.json
+$ npx qframe ./qframe.json # Note that you need the ./ here, the file is loaded using require()
+```
+
+## `mictest.js` example config
+
+The config file is loaded using `require()`, so you can go wild. Here's an example of setting up some custom API endpoints on a localhost HTTP/2 server.
+
+```javascript
+module.exports = {
+    // Web server port.
+    port: 8000,
+
+    // Override cluster worker count.
+    // The default is the number of logical CPUs (i.e. HyperThreads).
+    // workerCount: 1,
+
+    // Database Pool config.
+    // See node-postgres docs for details.
+    pg: {database: 'mictest'},
+
+    // Password salting rounds, see bcrypt docs.
+    // saltRounds: 10,
+
+    // The config object is passed straight to
+    // HTTP.createServer if there's no cert or pfx, or
+    // HTTP2.createSecureServer if cert or pfx is set.
+    // See the node HTTP/HTTP2 docs for details.
+    cert: "localhost-cert.pem",
+    key: "localhost-privkey.pem",
+    // pfx: "localhost.pfx",
+    // passphrase: "blabla",
+    allowHTTP1: true,
+
+    // Called at the end of a request
+    logAccess: function(req, status, elapsed) {
+        console.log([status, req.url, elapsed]);
+    },
+
+    // Called when a request errors out
+    logError: function(req, status, message) {
+        console.error([status, req.url, message]);
+    },
+
+    // root serves static files document root.
+    // All the files under this directory are cached to memory,
+    // once for each cluster worker.
+    // So, um, don't put large files here. 
+    // root: '/var/www/html',
+
+    // api adds routes under /_/
+    api: {
+        hello: async (req, res) => res.json({hello: "world"}),
+        echo: async (req, res) => res.end(await global.bodyAsBuffer(req)),
+        migrationLog: async (req, res) => {
+            await global.guardGetWithUser(req);
+            const { rows } = await global.DB.query(
+                `SELECT * FROM migration_log WHERE db_name = $1 ORDER BY id ASC`, [req.name]);
+            res.json(rows);
+        },
+        totalTaps: async (req, res) => {
+            const { rows: [taps] } = await global.DB.query(`SELECT SUM(taps) FROM mictest`);
+            res.json(taps);
+        }
+    },
+
+    // routes completely replaces the built-in routes
+    // routes: { foo: (_,res) => res.end('bar') }, // check out localhost:8000/foo
+
+    // Which migration to go to?
+    migrationTarget: 'testing testing 1-2-3',
+
+    // migrations adds to migrations
+    migrations: [
+        {
+            name: 'is this on?',
+            up: `CREATE TABLE mictest ( taps INT NOT NULL DEFAULT 1 );`,
+            down: `DROP TABLE mictest;`
+        },
+        {
+            name: 'testing testing 1-2-3',
+            up: `
+                INSERT INTO mictest (taps) VALUES (1);
+                INSERT INTO mictest (taps) VALUES (2);
+                INSERT INTO mictest (taps) VALUES (3);
+            `,
+            down: `DELETE FROM mictest;`
+        }
+    ]
+
+    // replaceMigrations replaces the built-in migrations
+    // replaceMigrations: [{ name: 'no need for db', up: 'select now()', down: 'select now()' }]
+};
+```
+
+To check it out, make some self-signed certs and create the `mictest` database.
+
+```bash
+$ openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' \
+  -keyout localhost-privkey.pem -out localhost-cert.pem
+$ createdb mictest
+$ npx qframe ./mictest.js
+```
+
+Test the new API endpoints from another shell.
+
+```bash
+$ curl -k https://localhost:8000/_/hello; echo
+{"hello":"world"}
+$ curl -k https://localhost:8000/_/totalTaps; echo
+{"sum":"6"}
+$ curl --data-binary "who is it" -k https://localhost:8000/_/echo; echo
+who is it
+```
+
+Then navigate to https://localhost:8000 and run the following in the console:
+
+```javascript
+var headers = {};
+var get = async(path) => (await fetch(path, {headers})).json();
+var post = async(path, body) => (await fetch(path, {method:'POST', headers, body: JSON.stringify(body)})).json();
+headers = await post('/_/user/create', {email:'foo@bar', name: 'foo', password: 'bar'});
+await get('/_/migrationLog/mictest')
 ```
--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const assert = require('assert');
 const HTTP = require('http');
+const HTTP2 = require('http2');
 const cluster = require('cluster');
+const { performance } = require('perf_hooks');
 const uuidv4 = require('uuid/v4');
 const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const fs = require('fs');
-const stream = require('stream');
 const path = require('path');
-const util = require('util');
-const mimedb = require('mime-db');
+const mime = require('mime');
+const chokidar = require('chokidar');
 const numCPUs = require('os').cpus().length;
-const [open, close, fstat] = [fs.open, fs.close, fs.fstat].map(util.promisify);
-const pipeline = util.promisify(stream.pipeline);
 
-const HTTP_SERVER_PORT = 8000;
-const saltRounds = 10;
-const DB = new Pool({database: 'qframe'});
+const config = {port: 8000, pg: {database: 'qframe'}, root: 'public', workerCount: numCPUs, saltRounds: 10,
+    ...(process.argv[2] && require(process.argv[2]) || JSON.parse(process.env.QFRAME_CONFIG || '{}'))};
+global.DB = new Pool(config.pg);
 
 // Migrations
-const Migrations = [{
+const migrations = config.replaceMigrations || [{
         name: 'Initial tables',
         up:`CREATE EXTENSION IF NOT EXISTS "pgcrypto";
             CREATE TABLE users (
@@ -28,45 +27,47 @@ const Migrations = [{
                 name TEXT NOT NULL UNIQUE,
                 email TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL,
-                data JSONB
-            );
+                data JSONB );
             CREATE TABLE sessions (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                 user_id UUID NOT NULL REFERENCES users(id),
                 created_time TIMESTAMP NOT NULL DEFAULT NOW(),
                 updated_time TIMESTAMP NOT NULL DEFAULT NOW(),
                 csrf TEXT NOT NULL,
-                data JSONB
-            );
+                data JSONB );
             CREATE TABLE items (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                 created_time TIMESTAMP NOT NULL DEFAULT NOW(),
                 updated_time TIMESTAMP NOT NULL DEFAULT NOW(),
                 user_id UUID NOT NULL REFERENCES users(id),
-                data JSONB
-            );`,
-        down: `DROP TABLE users, items, sessions;` }];
+                data JSONB );`,
+        down: `DROP TABLE users, items, sessions;` }, ...(config.migrations || [])];
 
 // Migration runner
-async function migrate(migrations, targetMigrationIndex=migrations.length-1) {
+async function migrate(migrations, migrationTarget) {
+    var targetMigrationIndex = migrations.findIndex(m => m.name === migrationTarget);
+    if (targetMigrationIndex === -1) targetMigrationIndex = migrationTarget || migrations.length-1; 
     const client = await DB.connect();
     try {
+        console.log(`Migration target`, [ client.database, targetMigrationIndex, (migrations[targetMigrationIndex] || {name:'EMPTY'}).name ]);
         await client.query('BEGIN');
-        await client.query(`CREATE TABLE IF NOT EXISTS migration (
-                db_name TEXT NOT NULL PRIMARY KEY UNIQUE,
-                latest_index INT NOT NULL DEFAULT -1,
-                latest_name TEXT )`);
+        await client.query(`CREATE TABLE IF NOT EXISTS migration (db_name TEXT NOT NULL PRIMARY KEY UNIQUE, latest_index INT NOT NULL DEFAULT -1, latest_name TEXT )`);
+        await client.query(`CREATE TABLE IF NOT EXISTS migration_log (id SERIAL PRIMARY KEY, db_name TEXT, created_time TIMESTAMP DEFAULT NOW(), name TEXT, direction TEXT, query TEXT, index INT );`);
         var { rows: [migrationStatus] } = await client.query('SELECT * FROM migration WHERE db_name = $1', [client.database]);
-        if (!migrationStatus) {
+        if (!migrationStatus)
             var { rows: [migrationStatus] } = await client.query("INSERT INTO migration (db_name) VALUES ($1) RETURNING *", [client.database]);
-        }
         assert(migrationStatus, 'Unable to create migration status database');
-        for (let i = migrationStatus.latest_index; i > targetMigrationIndex; i--)
+        for (let i = migrationStatus.latest_index; i > targetMigrationIndex; i--) {
             await client.query(migrations[i].down);
-        for (let i = migrationStatus.latest_index+1; i <= targetMigrationIndex; i++)
+            await client.query('INSERT INTO migration_log (db_name, name, direction, query, index) VALUES ($1, $2, $3, $4, $5)', [client.database, migrations[i].name, 'down', migrations[i].down, i]);
+        }
+        for (let i = migrationStatus.latest_index+1; i <= targetMigrationIndex; i++) {
             await client.query(migrations[i].up);
+            await client.query('INSERT INTO migration_log (db_name, name, direction, query, index) VALUES ($1, $2, $3, $4, $5)', [client.database, migrations[i].name, 'up', migrations[i].up, i]);
+        }
         await client.query('UPDATE migration SET latest_index = $1, latest_name = $2', [targetMigrationIndex, (migrations[targetMigrationIndex] || {}).name]);
         await client.query('COMMIT');
+        console.log(`Migration COMMIT`);
     } catch (err) {
         await client.query('ROLLBACK');
         throw err;
@@ -77,79 +78,72 @@ async function migrate(migrations, targetMigrationIndex=migrations.length-1) {
 
 // Routing
 async function route(routeObj, req, res) {
-    const path = req.url.split("/").slice(1);
-    for (var i = 0; i < path.length; i++) {
-        const currentDir = path[i];
-        const nextRouteObj = routeObj[currentDir];
-        if (!nextRouteObj) {
-            break;
-        } else if (typeof nextRouteObj === 'function') {
+    const path = req.url.split(/[\/\?]/);
+    for (var i = 1; i < path.length; i++) {
+        routeObj = routeObj[path[i]];
+        if (routeObj === undefined) return;
+        if (typeof routeObj === 'function') {
             req.name = path.slice(i+1).join("/");
-            return nextRouteObj(req, res);
+            return routeObj(req, res);
         }
-        routeObj = nextRouteObj;
     }
 }
 
 // File serving
-const extMimeTypes = {};
-for (let mime in mimedb) {
-    (mimedb[mime].extensions || []).forEach(ext => extMimeTypes[ext] = mime);
-}
-async function serveDirectory(baseDirectory, req, res) {
-    const reqPath = req.url.split("?")[0];
-    var fsPath = path.join(baseDirectory, path.normalize('/' + reqPath));
-    assert(fsPath.startsWith(path.normalize(baseDirectory + '/')), '403: Permission denied');
-    const isDir = /\/$/.test(reqPath);
-    try {
-        var fd = isDir ? null : await open(fsPath, 'r');
-        if (isDir || (await fstat(fd)).isDirectory()) {
-            if (fd !== null) close(fd); // do this concurrently
-            fsPath = path.join(fsPath, 'index.html');
-            fd = await open(fsPath, 'r');
+global.serveDirectory = function(baseDirectory) {
+    const cache = {};
+    chokidar.watch('.', {cwd: baseDirectory, ignored: /(^|[\/\\])\../}).on('all', async (_, filename) => {
+        delete cache['/'+filename];
+        fs.readFile(path.join(baseDirectory, filename), (_, buffer) =>
+            buffer && ( cache['/'+filename] = { buffer, headers: {'Content-Type': mime.getType(filename)} } ) );
+    });
+    return async function(req, res) {
+        const reqPath = req.url.split("?")[0];
+        const cached = cache[reqPath] || cache[path.join(reqPath, 'index.html')];
+        if (cached) {
+            res.writeHead(200, cached.headers);
+            res.end(cached.buffer);
         }
-    } catch(err) {
-        assert(!err.message.startsWith("ENOENT:"), "404: File not found");
-        assert(!err.message.startsWith("EACCES:"), "403: Permission denied");
-        throw err;
-    }
-    const mimetype = extMimeTypes[path.extname(fsPath).slice(1)] || 'application/octet-stream';
-    const fileStream = fs.createReadStream(fsPath, {fd});
-    res.writeHead(200, {'Content-Type': mimetype});
-    await pipeline(fileStream, res);
+    };
 }
 
 // Body JSON parsing
-HTTP.ServerResponse.prototype.json = function(obj, statusCode = 200) {
+HTTP.ServerResponse.prototype.json = HTTP2.Http2ServerResponse.prototype.json = function(obj, statusCode = 200) {
     this.writeHead(statusCode, {'Content-Type': 'application/json'});
-    this.end(JSON.stringify(obj));
+    this.end(JSON.stringify(obj))
 };
-const bodyAsBuffer = (req) => new Promise(function(resolve, reject) {
-    var chunks = [];
-    req.on('data', b => chunks.push(b));
-    req.on('end', () => resolve(Buffer.concat(chunks)));
-    req.on('error', reject);
+global.bodyAsBuffer = req => new Promise(function (resolve, reject) {
+    var index = 0, buffer = Buffer.allocUnsafe(4096);
+    req.on('data', function(b) {
+        index += b.byteLength;
+        if (index > buffer.byteLength) {
+            buffer = Buffer.allocUnsafe(1 << Math.ceil(Math.log2(index)));
+            buffer.set(buffer);
+        }
+        buffer.set(b, index - b.byteLength); });
+    req.on('end', function() { resolve(buffer.slice(0, index)) });
+    req.on('error', reject)
 });
-const bodyAsString = async req => (await bodyAsBuffer(req)).toString(); 
-const bodyAsJson = async req => JSON.parse(await bodyAsString(req));
+global.bodyAsString = async req => (await bodyAsBuffer(req)).toString(); 
+global.bodyAsJson = async req => JSON.parse(await bodyAsBuffer(req));
 
 // Authorization & CSRF guards
-async function guardPostWithSession(req) {
+global.guardPostWithSession = async function guardPostWithSession(req) {
     assert(req.method === 'POST', "405: Only POST accepted");
     const session = await sessionGet(req);
     assert(session, '401: User session invalid, please re-authenticate');
     assert(req.headers.csrf === session.csrf, '401: Invalid CSRF token, please re-authenticate');
-    return session;
+    return session
 }
-async function guardGetWithUser(req) {
+global.guardGetWithUser = async function guardGetWithUser(req) {
     const session = await sessionGet(req);
     assert(session, '401: User session invalid, please re-authenticate');
-    return session.user_id;
+    return session.user_id
 }
-async function guardPostWithUserAndJson(req) {
+global.guardPostWithUserAndJson = async function guardPostWithUserAndJson(req) {
     const sessionP = guardPostWithSession(req); // Run these two concurrently
     const jsonP = bodyAsJson(req);
-    return {user: (await sessionP).user_id, json: await jsonP}; 
+    return {user: (await sessionP).user_id, json: await jsonP}
 }
 
 // Session management
@@ -165,14 +159,14 @@ const sessionList = async (user_idId) =>
     (await DB.query(`SELECT id AS session, csrf, created_time, updated_time, data FROM sessions WHERE user_id = $1 ORDER BY created_time ASC`, [user_idId])).rows;
 
 // User accounts
-const User = {
+const user = {
     create: async function(req, res) {
         assert(req.method === 'POST', "405: Only POST accepted");
         const { email, password, name } = await bodyAsJson(req);
         assert(email, "400: No email provided");
         assert(password, "400: No password provided");
         assert(name, "400: No name provided");
-        const passwordHash = await bcrypt.hash(password, saltRounds);
+        const passwordHash = await bcrypt.hash(password, config.saltRounds);
         const { rows: [user] } = await DB.query('INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING id', [email, passwordHash, name]);
         assert(user, '500: User database insert failed');
         const session = await sessionCreate(user.id);        
@@ -237,7 +231,7 @@ const User = {
 };
 
 // Items
-const Items = {
+const items = {
     create: async function(req, res) {
         const {user, json} = await guardPostWithUserAndJson(req);
         const { rows } = await DB.query('INSERT INTO items (data, user_id) VALUES ($1, $2) RETURNING id, data, created_time, updated_time', [json, user]);
@@ -275,26 +269,32 @@ const Items = {
 };
 
 // Routes
-const Routes = { items: Items, user: User };
+const routes = config.routes || { _: {items, user, ...config.api} };
+const staticFile = serveDirectory(config.root);
 
 // HTTP Server
-const server = new HTTP.createServer(async function(req, res) {
+const serverConfig = {...config, cert: config.cert && fs.readFileSync(config.cert), key: config.key && fs.readFileSync(config.key)};
+const server = (config.cert || config.pfx ? HTTP2.createSecureServer : HTTP.createServer)(serverConfig, async function(req, res) {
+    const t0 = performance.now();
     try {
-        await route(Routes, req, res);
-        if (!res.finished) await serveDirectory('public/', req, res);
+        await route(routes, req, res);
+        if (!res.finished) await staticFile(req, res);
         assert(res.finished, "404: Route not found");
     } catch({message}) {
-        const statusCodeMatch = message.match(/^(\d{3})[^\d]/);
-        const status = statusCodeMatch ? parseInt(statusCodeMatch[1]) : 500;
+        const status = parseInt(message.slice(0,3)) || 500;
+        config.logError && config.logError(req, status, message);
         res.json({status, message}, status);
     }
+    config.logAccess && config.logAccess(req, res.statusCode, performance.now() - t0);
 });
 
 // Cluster
 if (cluster.isMaster) {
-    for (let i = 0; i < numCPUs; i++) cluster.fork();
-    cluster.on('exit', (worker, code, signal) => cluster.fork());
-    DB.connect().then(() => migrate(Migrations));
+    migrate(migrations, config.migrationTarget).then(() => {
+        for (let i = 0, l = config.workerCount; i < l; i++) cluster.fork();
+        cluster.on('exit', (worker, code, signal) => cluster.fork());
+    });
 } else {
-    DB.connect().then(() => server.listen(HTTP_SERVER_PORT, () => console.log(`[${process.pid}] Server running on ${HTTP_SERVER_PORT}`)));
-} // THIS IS SPARTA
+    DB.connect().then(() => server.listen(config.port, function() { console.log(`[${process.pid}] Server running on ${config.port}`) } ));
+}
+// THIS IS SPARTA
